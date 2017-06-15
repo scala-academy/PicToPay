e642ab0bb51c39ed4f76ea27a8eaf50dd3a95451
Bas Geerdink <bas@gdink.com>
2017-06-15 14:31:07 +0200

    project skeleton

---
+++ src/main/scala/domain/Domain.scala
@@ -0,0 +1,20 @@
+package domain
+
+case class Value(label: String, value: String)
+
+case class ValidatedValue(label: String, value: String, status: String)
+
+// FieldDetector --> OCR
+case class Field(label: String, picture: String)
+
+// OCR --> Collector
+case class RecognizedValue(Value)
+
+// Collector --> correction
+case class CollectedValues(list: List[Value])
+
+// correction --> Validator
+case class CorrectedValues(list: List[Value])
+
+// Validator --> OutputGenerator
+case class ValidatedValues(list: List[ValidatedValue])
---
+++ src/test/main/scala/MainSpec.scala
@@ -0,0 +1,6 @@
+package main.scala
+
+object MainSpec {
+
+  // TODO
+}
