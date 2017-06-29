0ba60da1af3be86dba5a0fec199a1c78285734de
NU <nsemeke@gmail.com>
2017-06-29 07:10:21 +0200

    cleaned up Orch spec

--- src/test/scala/orchestration/OrchestratorSpec.scala
+++ src/test/scala/orchestration/OrchestratorSpec.scala
@@ -4,13 +4,10 @@ import testUtils.BaseSpec
 
 class OrchestratorSpec extends BaseSpec {
 
-  " Orchestrator" should "throw an exception" in {
-    val thrown = intercept[Exception] {
-      val output = Orchestrator.run("test")
+  "Orchestrator" should "throw an exception when no file given" in {
+    //val thrown = intercept[Exception] {
+      val output = Orchestrator.run("")
       output should contain("NoSuchFileException")
-    }
+    //}
   }
-
-
-
 }
