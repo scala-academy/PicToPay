c277b1937ff6f713c10d142b0359fbb2b482459f
NU <nsemeke@gmail.com>
2017-07-03 10:32:30 +0200

    refactoring: named intermediate parameters and results to clarify it's usage

--- src/main/scala/detection/FieldDetector.scala
+++ src/main/scala/detection/FieldDetector.scala
@@ -5,26 +5,28 @@ import java.net.URL
 
 import com.sksamuel.scrimage.Image
 import com.sksamuel.scrimage.nio.PngWriter
-import domain.Field
+import domain.{Field, FieldCoordinates}
 
 
 object FieldDetector {
 
-  val fieldCoordinates = List(("IBAN", 190, 213, 121, 15), ("AmountOwed", 190, 59, 20, 15))
+  val fieldsCoordinates = List(
+    FieldCoordinates("IBAN", 190, 213, 121, 15),
+    FieldCoordinates("AmountOwed", 190, 59, 20, 15))
+
+  //override default writer compression config to gain max image quality
+  implicit val writer = PngWriter.NoCompression
 
   // Takes an image file and generates image files of different fields
   def map(img: URL): List[Field] = {
-
-    //override default writer compression config to gain max image quality
-    implicit val writer = PngWriter.NoCompression
     val path = img.getPath
     val image = Image.fromFile(new File(path))
 
-    fieldCoordinates.map(f => {
-      val label = f._1
-      val fieldImage = path.replace(path.substring(path.lastIndexOf("/")), "/generated" + label + ".png")
-      image.subimage(f._2, f._3, f._4, f._5).output(new File(fieldImage))
-      Field(label, fieldImage)
+    fieldsCoordinates.map(c => {
+      val l = c.label
+      val outputUrl = path.replace(path.substring(path.lastIndexOf("/")), "/generated" + l + ".png")
+      val createImage = image.subimage(c.x, c.y, c.w, c.h).output(new File(outputUrl))
+      Field(l, outputUrl)
     })
   }
 }
