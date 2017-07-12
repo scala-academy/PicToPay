3cb064e89aca6e9924cf7ba3e00fe214e83be959
Ger van Rossum <gvr@users.noreply.github.com>
2017-07-12 10:05:35 +0200

    cleaned validator code

--- src/main/scala/validation/IbanValidator.scala
+++ src/main/scala/validation/IbanValidator.scala
@@ -1,12 +1,10 @@
 package validation
 
-/**
-  * Created by GK46IV on 15-6-2017.
-  */
 object IbanValidator {
+
   def validate(iban: String): Status = {
     if (checkSumCheck(iban)) Valid
-//    else if (correctIban(iban)) Corrected
+    //  else if (correctIban(iban)) Corrected
     else Invalid
   }
 
@@ -16,4 +14,5 @@ object IbanValidator {
     val encodedIban = shuffledIban.map(ch => if (ch.isDigit) ch.toInt - '0'.toInt else ch - 'A'.toInt + 10).mkString
     (BigInt(encodedIban) mod 97) == 1
   }
+
 }
