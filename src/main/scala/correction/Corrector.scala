813f70f6496e428c0178d58553e49e5398d03aeb
geerdink <bas@gdink.com>
2017-07-13 14:57:11 +0200

    added witespace removal function

--- src/main/scala/correction/Corrector.scala
+++ src/main/scala/correction/Corrector.scala
@@ -1,7 +1,36 @@
 package correction
 
-import domain.{CollectedValues, CorrectedValues}
+import domain._
 
 object Corrector {
-  def map(values: CollectedValues): CorrectedValues = CorrectedValues(values.list)
+  def map(values: CollectedValues): CorrectedValues = {
+    CorrectedValues(
+      values.list.map(v =>
+        v.label match {
+          case "IBAN" => Value(v.label, correctIban(v.value))
+          case _ => Value(v.label, v.value)
+        }
+      )
+    )
+  }
+
+  def correctIban(iban: String): String = {
+    // remove white space
+    correctIbanWhiteSpace(iban)
+
+    //correctIbanWhiteSpace(iban)
+
+    // apply chars -> numbers rules
+
+    // apply numbers -> chars rules
+
+    // use checksum.
+    // what happens if no valid IBAN can be found??
+  }
+
+  def correctIbanWhiteSpace(iban: String): String = {
+    iban.trim.replaceAll(" ", "")
+  }
+
 }
+
--- src/test/scala/correction/CorrectorSpec.scala
+++ src/test/scala/correction/CorrectorSpec.scala
@@ -4,11 +4,28 @@ import domain._
 import testUtils.BaseSpec
 
 class CorrectorSpec extends BaseSpec {
+  val whiteSpaceIban = " NL77 INGB 0123456 78 "
+  val correctIban    = "DE89370400440532013000"
+  val incorrectIban  = "D3B937O40O4A0S320l30o0 "
 
-  "Corrector" should "correct an incorrect IBAN number" in {
-    val iban = "NL77INGB012345678"
-    val corrected = Corrector.map(CollectedValues(List(Value("IBAN", iban))))
+  // TODO: check country specific rules!
 
-    corrected.list.size should be(1)
+  "Corrector" should "pick out the IBAN numbers to correct" in {
+    val corrected = Corrector.map(CollectedValues(List(
+      Value("IBAN", correctIban),
+      Value("something else", "useless"))))
+
+    corrected.list.size should be(2)
+
+    corrected.list(0).label should be("IBAN")
+    corrected.list(1).label should not be("IBAN")
+  }
+
+  it should "remove the white space from an IBAN number" in {
+
+  }
+
+  it should "fix an incorrect IBAN number" in {
+
   }
 }
